dae7bbf891376f59dfa1687a7f65302b6cbe2fc6
Joe <jconstan@bu.edu>
2016-07-05 21:11:53 +0900

    h

--- app/src/main/java/com/example/joseph/safewalk2/MainActivity.java
+++ app/src/main/java/com/example/joseph/safewalk2/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-
+//ji
 public class MainActivity extends AppCompatActivity {
 
     @Override
