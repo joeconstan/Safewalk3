a9a40e05513a7f2f7afe8d9d869230d98843940b
Joe <jconstan@bu.edu>
2016-07-05 20:59:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/joseph/safewalk2/Details.java
@@ -0,0 +1,19 @@
+package com.example.joseph.safewalk2;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Details extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/joseph/safewalk2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.joseph.safewalk2;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button requestWalk = (Button) findViewById(R.id.walkButton);
+        requestWalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Details.class); //rename
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+
+
+}
