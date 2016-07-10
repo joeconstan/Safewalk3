75189aeb0494ca8178b3afb0df82634ca071e501
Joe <jconstan@bu.edu>
2016-07-10 15:06:41 +0900

    progress.4

--- app/src/main/java/com/example/joseph/safewalk2/Register.java
+++ app/src/main/java/com/example/joseph/safewalk2/Register.java
@@ -1,8 +1,13 @@
 package com.example.joseph.safewalk2;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity{
 
@@ -10,6 +15,22 @@ public class Register extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //register them first
+                Intent in1 = new Intent(v.getContext(), MainActivity.class); //orrr??
+                startActivity(in1);
+                Context context = getApplicationContext();
+                CharSequence text = "Account Created!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+        });
 
     }
 
