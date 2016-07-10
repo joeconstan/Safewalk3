fa904ba90fa6a314e4fcdbd157f1dd94d497c19f
Alfredo Garcia <suitcase@bu.edu>
2016-07-10 16:38:03 -0400

    Added the spinner but trying to change the color properly, I changed the backy to white because the text would stay black. Next imma add a small description and implement the ability to send an sms message. also ur gay tell me sooner before so i can plan to wake early

--- app/src/main/java/com/example/joseph/safewalk2/Buildous.java
+++ app/src/main/java/com/example/joseph/safewalk2/Buildous.java
@@ -3,6 +3,11 @@ package com.example.joseph.safewalk2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class Buildous extends AppCompatActivity {
 
@@ -11,7 +16,23 @@ public class Buildous extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buildous_deliverous);
 
+        Spinner spinner = (Spinner) findViewById(R.id.buildingSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.buildingsArray, android.R.layout.simple_spinner_item);
 
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) = " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
