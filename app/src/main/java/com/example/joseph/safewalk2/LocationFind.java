b7383ca4e1bea8ff5ce50512cf5c7278cc1d64a9
Joe <jconstan@bu.edu>
2016-07-12 14:10:44 +0900

    progress.7

--- app/src/main/java/com/example/joseph/safewalk2/LocationFind.java
+++ app/src/main/java/com/example/joseph/safewalk2/LocationFind.java
@@ -1,6 +1,10 @@
 package com.example.joseph.safewalk2;
 
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +14,29 @@ public class LocationFind extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_find);
 
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }
+                ;
 
 
 
