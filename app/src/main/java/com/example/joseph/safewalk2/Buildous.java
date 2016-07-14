a3a61559aa993246b076e7496765e027cd101d69
Alfredo Garcia <suitcase@bu.edu>
2016-07-13 22:52:04 -0400

    I added some sms functionality to it but it crashes immediately when i try to run it on my phone

--- app/src/main/java/com/example/joseph/safewalk2/Buildous.java
+++ app/src/main/java/com/example/joseph/safewalk2/Buildous.java
@@ -1,15 +1,21 @@
 package com.example.joseph.safewalk2;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Buildous extends AppCompatActivity {
+
+    public String txtSelected = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,7 @@ public class Buildous extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Object test = parent.getItemAtPosition(position);
-                String test2 = test.toString();
+                txtSelected = test.toString();
             }
 
             @Override
@@ -41,6 +47,29 @@ public class Buildous extends AppCompatActivity {
         });
 
         //Sending the building
+        sendBuilding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txtView = (TextView) findViewById(R.id.txtTest);
+                txtView.setText(txtSelected);
+
+                //Sending the sms message
+                Log.i("Send SMS", "");
+                String phoneNo = "3054580416";
+                String message = "Holy fuck it worked";
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+                }
+
+                catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 }
