444603a4ffc6ea951985e06ce6941fa44d57c875
Joe <jconstan@bu.edu>
2016-08-01 21:30:36 -0700

    help

--- app/src/main/java/com/example/joseph/safewalk2/MainActivity.java
+++ app/src/main/java/com/example/joseph/safewalk2/MainActivity.java
@@ -2,18 +2,39 @@
 
 package com.example.joseph.safewalk2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            boolean f = checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED;
+            if (f)
+                Log.v(TAG, "Permission is granted");
+            else {
+                Log.v(TAG, "Permission not granted");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
+            }
+        }
+
+
 
         Button requestWalk = (Button) findViewById(R.id.walkButton);
         requestWalk.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+          //  Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+       // }
     }
 
 
