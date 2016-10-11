2acd7502660dde03cc6ef686e64de2ba3d2b38ed
largepotatoes <LxFF64?short>
2016-10-10 21:48:45 -0400

    started sms but needs to change

--- app/src/main/java/com/example/joseph/safewalk2/LocationDeliverous.java
+++ app/src/main/java/com/example/joseph/safewalk2/LocationDeliverous.java
@@ -1,27 +1,42 @@
 package com.example.joseph.safewalk2;
 
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class LocationDeliverous extends AppCompatActivity{
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_deliverous);
 
+        if (Build.VERSION.SDK_INT >= 23) {
+            boolean x = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
 
+            if (x)
+                Log.v(TAG, "Permission is granted");
+            else {
+                Log.v(TAG, "Permission not granted. requesting...");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1); //here
+            }
+        }
         Button deliverousSubmitButton = (Button) findViewById(R.id.deliverousSubmitButton);
         deliverousSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
 
 
                 SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
@@ -42,6 +57,13 @@ public class LocationDeliverous extends AppCompatActivity{
             }
         });
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+    }
+
 
 }
 
